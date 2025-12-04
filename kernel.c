89561967ebf7196480ccc82bcf3837701635d26f
Rvux86-64 <hamza_siddiqui2023@hotmail.com>
2025-12-04 07:48:20 -0600

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,9 @@
+#include <efi.h>
+#include <efilib.h>
+EFI_STATUS EFIAPI
+efi_main (EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
+{
+    InitializeLib(image_handle, system_table);
+    Print((CHAR16*)L"Hello from the UEFI\n");
+    return EFI_SUCCESS;
+}
